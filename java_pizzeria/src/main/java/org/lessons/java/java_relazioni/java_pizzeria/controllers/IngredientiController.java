d5d35732976ddc12ae3bc196c937fb42e8508ca0
JacopoTrincanato <trincanatoj@gmail.com>
2025-03-31 18:22:59 +0200

    implements create and edit routes in IngredientiController.java

--- java_pizzeria/src/main/java/org/lessons/java/java_relazioni/java_pizzeria/controllers/IngredientiController.java
+++ java_pizzeria/src/main/java/org/lessons/java/java_relazioni/java_pizzeria/controllers/IngredientiController.java
@@ -1,12 +1,18 @@
 package org.lessons.java.java_relazioni.java_pizzeria.controllers;
 
+import org.lessons.java.java_relazioni.java_pizzeria.models.Ingredienti;
 import org.lessons.java.java_relazioni.java_pizzeria.repositories.IngredientiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ingredienti")
@@ -27,5 +33,60 @@ public class IngredientiController {
     public String show(@PathVariable Integer id, Model model) {
         model.addAttribute("ingrediente", ingredientiRepository.findById(id).get());
         return "ingredienti/show";
+    }
+
+    // rotta create
+    @GetMapping("/create")
+    public String create(Model model) {
+        // creo il nuovo ingrediente
+        model.addAttribute("ingrediente", new Ingredienti());
+
+        // prendo tutti gli ingredienti
+        model.addAttribute("ingredienti", ingredientiRepository.findAll());
+
+        return "ingredienti/create-or-edit";
+    }
+
+    // rotte store (crea effettivamente il nuovo ingrediente)
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("ingrediente") Ingredienti ingredienteForm, BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "ingredienti/create-or-edit";
+        }
+
+        ingredientiRepository.save(ingredienteForm);
+
+        return "redirect:/ingredienti";
+    }
+
+    // rotta edit
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+
+        // prendo tutti gli ingredienti
+        model.addAttribute("ingredienti", ingredientiRepository.findAll());
+
+        // trovo l'ingrediente per id
+        model.addAttribute("ingrediente", ingredientiRepository.findById(id).get());
+
+        model.addAttribute("edit", true);
+
+        return "ingredienti/create-or-edit";
+    }
+
+    // rotte update (aggiorna effettivamente l'ingrediente)
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("ingrediente") Ingredienti ingredienteForm, BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "ingredienti/create-or-edit";
+        }
+
+        ingredientiRepository.save(ingredienteForm);
+
+        return "redirect:/ingredienti";
     }
 }
